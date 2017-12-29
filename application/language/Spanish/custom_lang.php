6541f94223e3def814a06d7a020a1f01c1851c70
Rodrigo Spirolazzi <rspirolazzi@gmail.com>
2017-12-29 11:31:14 -0300

    Nuevas traducciones

---
+++ application/language/Spanish/custom_lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * CUSTOM LANGUAGE STRINGS
+ *
+ * Add your custom Language Strings here with the following scheme:
+ *
+ * 'language_string_key' => 'Actual content of the Language String',
+ *
+ * Please notice that 'language_string_key' should only contain alphanumeric
+ * characters and underscores.
+ * If you are not sure take a look at the ip_lang.php file or visit the
+ * community forums.
+ */
+
+$lang = array(
+    'service_date' => 'Fecha de Reparación',
+    'service' => 'Reparación',
+    'services' => 'Reparaciones',
+    'create_service' => 'Crear Reparación',
+    'view_services' => 'Ver Reparaciones',
+    'enable_invoices' => 'Habilitar Facturación',
+    'service_overview' => 'Resumen de reparaciones',
+);
